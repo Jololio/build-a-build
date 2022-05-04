640116966c775a9a18748404608e02b21fcdc2e1
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-05-04 17:15:23 -0600

    got a good start on the build page

---
+++ public/buildPage.js
@@ -0,0 +1,3 @@
+const usernameHeader = document.getElementById('username_header')
+
+usernameHeader.innerText = window.localStorage.getItem('username') + "'s Builds:" 
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,3 +1,6 @@
+const loginForm = document.getElementById('login_form')
+const usernameInput = document.getElementById('username_input')
+
 const showRegisterForm = () => {
     document.getElementById('register_form').style.display = 'block'
     document.getElementById('login_form').style.display = 'none'
@@ -6,3 +9,10 @@ const showLoginForm = () => {
     document.getElementById('login_form').style.display = 'block'
     document.getElementById('register_form').style.display = 'none'
 }
+
+login_form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    window.localStorage.setItem('username', usernameInput.value)
+    window.location.href = './buildPage.html'
+})
