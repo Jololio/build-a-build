3c23ca6a0d9c797fbbe8fdc7ec987f9ac193cbce
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-05-09 21:43:25 -0600

    finally got the buttons to work and look good

--- public/buildPage.js
+++ public/buildPage.js
@@ -3,6 +3,8 @@ let champDropdown = document.getElementById('champions')
 let dropdownArr = [document.getElementById('item_dropdwn1'), document.getElementById('item_dropdwn2'), document.getElementById('item_dropdwn3'), document.getElementById('item_dropdwn4'), document.getElementById('item_dropdwn5'), document.getElementById('item_dropdwn6')]
 let buildNamesContainer = document.getElementById('build_names_container')
 let buildNameInput = document.getElementById('build_name_input')
+let viewBuildContainer = document.getElementById('view_build_container')
+let currentBuild = null;
 
 usernameHeader.innerText = window.localStorage.getItem('username') + "'s Builds:" 
 
@@ -19,7 +21,6 @@ const getChampions = () => {
 }
 getChampions()
 const getItems = () => {
-    console.log(dropdownArr, champDropdown)
     axios.get('http://localhost:9876/items').then(res => {
         dropdownArr.forEach(dropdown => {
             res.data.forEach(item => {
@@ -31,6 +32,7 @@ const getItems = () => {
     })
 }
 getItems()
+
 const saveBuild = () => {
     let body= {
         buildName: buildNameInput.value,
@@ -45,107 +47,77 @@ const saveBuild = () => {
             dropdownArr[5].value
         ]
     }
-    console.log(body)
-    axios.post('http://localhost:9876/create-build', body).then(res => {
-        console.log(res)
+    if(currentBuild) {
+        // Build Update Endpoint
+        body.buildId = currentBuild.build_id
+        axios.put('http://localhost:9876/edit-build', body).then(res => {
+            alert('Build Updated!')
+            getBuildNames()
+        })
+    }
+    else {
+        axios.post('http://localhost:9876/create-build', body).then(res => {
+            getBuildNames()
+        })
+    }
+}
+
+const showBuildContainer = () => {
+    let editorContainer = document.getElementById('editor_container')
+    viewBuildContainer.style.display = 'block'
+    editorContainer.style.display = 'none'
+}
+const getBuildItems = (build, isEditing) => {
+    console.log(build)
+    let {item_1, item_2, item_3, item_4, item_5, item_6, champ_name, build_name} = build
+    let editorContainer = document.getElementById('editor_container')
+    axios.get(`http://localhost:9876/items?item_1=${item_1}&item_2=${item_2}&item_3=${item_3}&item_4=${item_4}&item_5=${item_5}&item_6=${item_6}`).then(res => {
+        document.getElementById(`build_name_header`).innerHTML = build_name
+        document.getElementById(`champ_name_header`).innerHTML = champ_name
+        res.data.forEach((item, i) => {
+            document.getElementById(`build_item${i+1}`).innerHTML = item.item_name  
+        })
+
+        if(isEditing) {
+            champDropdown.value = build.champion_id
+            buildNameInput.value = build.build_name
+            for (let i = 0; i < res.data.length; i++) {
+              dropdownArr[i].value = res.data[i].item_id
+            }
+        }
     })
 }
-const getBuildItems = (build) => {
-    console.log(build)
-    let {item_1, item_2, item_3, item_4, item_5, item_6} = build
-    axios.get(`http://localhost:9876/items?item_1=${item_1}&item_2=${item_2}&item_3=${item_3}&item_4=${item_4}&item_5=${item_5}&item_6=${item_6}`).then(res => {
-        console.log(res.data)
-        let buildNameDisplay = document.getElementById('build_name_display')
-        let championNameDisplay = document.getElementById('champion_name_display')
-        for (let i = 0; i < res.data.length; i++){
-            let itemContainer = document.getElementById(`item${i+1}_container`)
-            itemContainer.innerHTML = `<h1>${res.data[i].item_name}</h1>`
-        }
-        buildNameDisplay.innerHTML = `<h1>${build.build_name}</h1>`
-        championNameDisplay.innerHTML = `<h1>${build.champ_name}</h1>`
-    })
+const showEditor = () => {
+    let editorContainer = document.getElementById('editor_container')
+    viewBuildContainer.style.display = 'none'
+    editorContainer.style.display = 'block'
 } 
 const getBuildNames = () => {
     axios.get('http://localhost:9876/builds').then(res => {
-        console.log(res.data)
         let buildNamesContainer = document.getElementById('build_names_container')
-        console.log(buildNamesContainer)
+        buildNamesContainer.innerHTML = ''
         for (let i = 0; i < res.data.length; i++){
-            console.log(res.data[i])
             let buildName = document.createElement('h1')
+            buildName.style.cursor = 'pointer'
             buildName.textContent = res.data[i].build_name
             buildNamesContainer.appendChild(buildName)
             buildName.addEventListener('click', () => {
+                showBuildContainer()
+                currentBuild = res.data[i]
                 getBuildItems(res.data[i])
             })
         }
     })
 }
 getBuildNames()
-const insertBuildForm = () => {
-    let editorContainer = document.getElementById('editor_container')
-    editorContainer.innerHTML = `<h1>CREATE A NEW BUILD:</h1>
-    <div id="build_name_display">
-      <input id="build_name_input" placeholder="Name your build">
-    </div>
-  <div id="champion_name_display">
-    <label for="champions">Choose a champion</label>
-    <select name="champions" id="champions">
-      <option value="" style="display: none">Choose a champion</option>
-    </select>
-  </div>
-
-    <section class="dropdown_container">
-      <div id="item1_container">
-      <label for="item_dropdwn1">First Item</label>
-      <select name="item_dropdwn1" id="item_dropdwn1">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-      </div>
-
-      <div id="item2_container">
-      <label for="item_dropdwn2">Second Item</label>
-      <select name="item_dropdwn2" id="item_dropdwn2">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-    </div>
-    </section>
-     
-    <section class="dropdown_container">
-      <div id="item3_container">
-      <label for="item_dropdwn3">Third Item</label>
-      <select name="item_dropdwn3" id="item_dropdwn3">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-      </div>
-      
-      <div id="item4_container">
-      <label for="item_dropdwn4">Fourth Item</label>
-      <select name="item_dropdwn4" id="item_dropdwn4">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-      </div>
-    </section>
-
-    <section class="dropdown_container">
-      <div id="item5_container">
-      <label for="item_dropdwn5">Fifth Item</label>
-      <select name="item_dropdwn5" id="item_dropdwn5">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-      </div>
-
-      <div id="item6_container">
-      <label for="item_dropdwn6">Sixth Item</label>
-      <select name="item_dropdwn6" id="item_dropdwn6">
-        <option value="" style="display: none">Choose an item</option>
-      </select>
-    </div>
-    </section>
-    
-
-    <button id="save_button" onclick="saveBuild()">Save Build</button>`
-    getChampions()
-    getItems()
+const deleteBuild = () => {
+    console.log(currentBuild)
+    axios.delete(`http://localhost:9876/delete-build/${currentBuild.build_id}`).then(res => {
+        alert('Build has been deleted!')
+        getBuildNames()
+    })
 }
-document.getElementById('create_button').addEventListener('click', insertBuildForm)
+const editBuild = () => {
+    showEditor()
+    getBuildItems(currentBuild, true)
+}
--- server/index.js
+++ server/index.js
@@ -48,13 +48,18 @@ app.get('/items', (req, res) => {
             FROM bab_items
             WHERE item_id IN (${item_1}, ${item_2}, ${item_3}, ${item_4}, ${item_5}, ${item_6});
         `).then(dbRes => {
-            res.status(200).send(dbRes[0])
+            console.log(dbRes[0])
+            const sortedItems = Object.values(req.query).map(item =>{
+                return dbRes[0].find(i => i.item_id == item)
+            })
+            res.status(200).send(sortedItems)
         })
     } else {
         sequelize.query(`
         SELECT *
         FROM bab_items
         `).then(dbRes => {
+            console.log(dbRes[0])
         res.status(200).send(dbRes[0])
         })
     }
@@ -64,7 +69,28 @@ app.post('/create-build', (req, res) => {
     sequelize.query(`
         INSERT INTO bab_builds (build_name, user_id, champion_id, item_1, item_2, item_3, item_4, item_5, item_6)
         VALUES('${req.body.buildName}', '${req.body.userId}', ${req.body.champion}, ${req.body.items[0]}, ${req.body.items[1]}, ${req.body.items[2]}, ${req.body.items[3]}, ${req.body.items[4]}, ${req.body.items[5]})   
-    `)
+    `).then(dbRes => {
+        res.status(200).send(dbRes[0])
+    })
+})
+
+app.delete(`/delete-build/:buildId`, (req, res) => {
+    sequelize.query(`
+        DELETE FROM bab_builds
+        WHERE build_id = ${req.params.buildId}
+    `).then(dbRes => {
+        res.status(200).send(dbRes[0])
+    })
+})
+
+app.put(`/edit-build`, (req,res) => {
+    sequelize.query(`
+        UPDATE bab_builds
+        SET build_name = '${req.body.buildName}', champion_id = ${req.body.champion}, item_1 = ${req.body.items[0]}, item_2 = ${req.body.items[1]}, item_3 = ${req.body.items[2]}, item_4 = ${req.body.items[3]}, item_5 = ${req.body.items[4]}, item_6 = ${req.body.items[5]}
+        WHERE build_id = ${req.body.buildId} 
+    `).then(dbRes => {
+        res.status(200).send(dbRes[0])
+    })
 })
 
 app.get('/builds', getUserBuilds)
