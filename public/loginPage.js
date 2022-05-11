5995eb794e2083d006f2f91233f36fa8b9848049
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-05-10 23:23:33 -0600

    login functionality maybe

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById('login_form')
 const usernameInput = document.getElementById('username_input')
+const passwordInput = document.getElementById('password_input')
 
 const showRegisterForm = () => {
     document.getElementById('register_form').style.display = 'block'
@@ -13,7 +14,11 @@ const showLoginForm = () => {
 login_form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    window.localStorage.setItem('username', usernameInput.value)
-    window.localStorage.setItem('userId', 1)
-    window.location.href = './buildPage.html'
+    axios.get(`https://build-a-build-jala0128.herokuapp.com/login/${usernameInput.value}/${passwordInput.value}`).then(res => {
+        window.localStorage.setItem('username', usernameInput.value)
+        window.localStorage.setItem('userId', res.data.user_id)
+        window.location.href = './buildPage.html'
+        console.log(res.data)
+    })
+    
 })
